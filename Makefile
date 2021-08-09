b5a59c2505016ae3a7e71de7cbb3e9560c2c2e94
junjieliu <junjieliu2910@gmail.com>
2021-08-09 14:54:47 +0800

    change to default

--- Makefile
+++ Makefile
@@ -2,37 +2,47 @@ GCC = riscv32-unknown-elf-gcc
 DUMP = riscv32-unknown-elf-objdump
 LINK = riscv32-unknown-elf-gcc
 COPY = riscv32-unknown-elf-objcopy
-
-SRCDIR = ./src
-OBJDIR = ./obj
-HEADERDIR = ./src
-# BINARY = lorawan_riscv
-SOURCES = $(wildcard $(SRCDIR)/*.c)
-HEADERS = $(wildcard $(SRCDIR)/*.h)
-OBJECTS = $(patsubst $(SRCDIR)/%.c, $(OBJDIR)/%.o, $(SOURCES))
-
+#LINK = riscv32-unknown-elf-ld
+SRC = ./src
+OBJ = ./obj
+SRCFILES = $(SRC)/aes.c  $(SRC)/LoRaWan.c $(SRC)/sleep.c  \
+	$(SRC)/LoRaMacCrypto.c   $(SRC)/utilities.c \
+	$(SRC)/cmac.c $(SRC)/io.c $(SRC)/main.c $(SRC)/sx1262.c $(SRC)/sc_print.c $(SRC)/curve25519-donna.c
+HDRFILES = $(SRC)/cmac.h $(SRC)/io.h $(SRC)/sx1262.h $(SRC)/aes.h \
+	$(SRC)/glb_include.h $(SRC)/LoRaWan.h $(SRC)/sleep.h $(SRC)/LoRaMacCrypto.h $(SRC)/utilities.h $(SRC)/sc_print.h $(SRC)/ecdh.h
+OBJFILES = $(OBJ)/main.o $(OBJ)/utilities.o $(OBJ)/io.o $(OBJ)/sleep.o $(OBJ)/aes.o $(OBJ)/cmac.o $(OBJ)/LoRaMacCrypto.o \
+	$(OBJ)/sx1262.o $(OBJ)/LoRaWan.o $(OBJ)/sc_print.o $(OBJ)/curve25519-donna.o $(OBJ)/ecdh.o
 LDSCRIPT = link.riscv.ld
+
 ELFFILE  = lorawan_riscv.elf
+
 DUMPFILE = lorawan_riscv.dump
 DUMPCOEFILE = $(addsuffix _dump.coe, $(basename $(DUMPFILE)))
+
 S19FILE  = lorawan_riscv.s19
 ICOEFILE = lorawan_riscv_instram.coe
 DCOEFILE = lorawan_riscv_dataram.coe
+
 MEMFILE = lorawan_riscv.mem
 
-build: main.o $(ELFFILE)
+build: compile link
 
+link: $(OBJFILES)
+	$(LINK) -o $(ELFFILE) $(OBJFILES) -T $(LDSCRIPT) -nostartfiles -nostdlib
 
-$(OBJDIR)/%.o: $(SRCDIR)/%.c
-	@mkdir -p $(@D)
-	$(GCC) -c $< -o $@ 
-
-main.o: main.c
-	$(GCC) -c main.c -o main.o 
-
-$(ELFFILE): $(OBJECTS)
-	$(LINK) -o $(ELFFILE) main.o $(OBJECTS) -T $(LDSCRIPT) -nostartfiles -nostdlib
-
+compile: $(SRCFILES) $(HDRFILES)
+	$(GCC) -c $(SRC)/main.c -o $(OBJ)/main.o
+	$(GCC) -c $(SRC)/LoRaWan.c -o $(OBJ)/LoRaWan.o
+	$(GCC) -c $(SRC)/sx1262.c -o $(OBJ)/sx1262.o
+	$(GCC) -c $(SRC)/LoRaMacCrypto.c -o $(OBJ)/LoRaMacCrypto.o
+	$(GCC) -c $(SRC)/cmac.c -o $(OBJ)/cmac.o
+	$(GCC) -c $(SRC)/aes.c -o $(OBJ)/aes.o
+	$(GCC) -c $(SRC)/utilities.c -o $(OBJ)/utilities.o
+	$(GCC) -c $(SRC)/io.c -o $(OBJ)/io.o
+	$(GCC) -c $(SRC)/sleep.c -o $(OBJ)/sleep.o
+	$(GCC) -c $(SRC)/sc_print.c -o $(OBJ)/sc_print.o
+	$(GCC) -c $(SRC)/curve25519-donna.c -o $(OBJ)/curve25519-donna.o
+	$(GCC) -c $(SRC)/ecdh.c -o $(OBJ)/ecdh.o
 dump: $(ELFFILE)
 	$(DUMP) -dC $(ELFFILE) > $(DUMPFILE)
 
@@ -54,3 +64,4 @@ mem: $(S19FILE)
 .PHONY: clean
 clean:
 	rm -rf lorawan_riscv* obj/*
+
--- src/main.c
+++ src/main.c
@@ -47,31 +47,31 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include "src/glb_include.h"
-#include "src/sleep.h"
-#include "src/sx1262.h"
-#include "src/LoRaWan.h"
-#include "src/ecdh.h"
-#include "src/sc_print.h"
+#include "glb_include.h"
+#include "sleep.h"
+#include "sx1262.h"
+#include "LoRaWan.h"
+//#include "ecdh.h"
+#include "sc_print.h"
 
-uint8_t devEui[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
-uint8_t appEui[8] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88 };
-uint8_t appKey[16] = { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0x0f, 0xed, 0xcb, 0xa9, 0x87, 0x65, 0x43, 0x21 };
+// uint8_t devEui[8] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+// uint8_t appEui[8] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88 };
+// uint8_t appKey[16] = { 0x12, 0x34, 0x56, 0x78, 0x9a, 0xbc, 0xde, 0xf0, 0x0f, 0xed, 0xcb, 0xa9, 0x87, 0x65, 0x43, 0x21 };
 
-uint8_t ecdh_pri_key[32];
-uint8_t ecdh_pub_key[32];
-uint8_t ecdh_shared_key[32];
-bool ecdh_shared_key_exist = false;
+// uint8_t ecdh_pri_key[32];
+// uint8_t ecdh_pub_key[32];
+// uint8_t ecdh_shared_key[32];
+// bool ecdh_shared_key_exist = false;
 
 
-// Hard-coded private for test
-char pri_key_string[64] = "584B6574AAF49F7A6A7872E49B8A31F1A982BE27FD366D84FEEE1BEB30DF747D";
-char other_pub_string[64] = "b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f";
+// // Hard-coded private for test
+// char pri_key_string[64] = "584B6574AAF49F7A6A7872E49B8A31F1A982BE27FD366D84FEEE1BEB30DF747D";
+// char other_pub_string[64] = "b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f";
 
-void string_to_byte_convert(char s[], uint8_t addressBytes[32]);
-void ecdh_packet_process(uint8_t *msg);
-void test_ecdh(uint8_t *pri_key, uint8_t *other_pub);
-void test_message(uint8_t *msg);
+// void string_to_byte_convert(char s[], uint8_t addressBytes[32]);
+// void ecdh_packet_process(uint8_t *msg);
+// void test_ecdh(uint8_t *pri_key, uint8_t *other_pub);
+// void test_message(uint8_t *msg);
 
 int main()
 {
@@ -143,67 +143,67 @@ int main()
 }
 
 
-void ecdh_packet_process(uint8_t *msg){
-    /*******
-    Format of the packet between server and device:
-    | length (1 byte) | indicator (1 byte) | msg |
-    *******/ 
+// void ecdh_packet_process(uint8_t *msg){
+//     /*******
+//     Format of the packet between server and device:
+//     | length (1 byte) | indicator (1 byte) | msg |
+//     *******/ 
 
-    // packet offset 
-    uint8_t front_offset = 9;
-    uint8_t msg_length = msg[front_offset];
+//     // packet offset 
+//     uint8_t front_offset = 9;
+//     uint8_t msg_length = msg[front_offset];
 
-    if(msg[front_offset+1] == 0xff){
-        uint8_t *other_pub_key = msg + front_offset + 2; 
+//     if(msg[front_offset+1] == 0xff){
+//         uint8_t *other_pub_key = msg + front_offset + 2; 
 
-        generate_curve25519_shared_key(ecdh_shared_key, ecdh_pri_key, other_pub_key);
-        sc_printf("Server requst for ECDH, server public key is: \n");
-        for(int i = 0; i < 32; ++i){
-            sc_printf("%02hhX", *(other_pub_key+i));
-        }
-        sc_printf("\n");
-        sc_printf("The shared secret key is: \n");
-        for(int i = 0; i < 32; ++i){
-            sc_printf("%02hhX", *(ecdh_shared_key+i));
-        }
-        sc_printf("\n");
-        ecdh_shared_key_exist = true;
-    }
-}
+//         generate_curve25519_shared_key(ecdh_shared_key, ecdh_pri_key, other_pub_key);
+//         sc_printf("Server requst for ECDH, server public key is: \n");
+//         for(int i = 0; i < 32; ++i){
+//             sc_printf("%02hhX", *(other_pub_key+i));
+//         }
+//         sc_printf("\n");
+//         sc_printf("The shared secret key is: \n");
+//         for(int i = 0; i < 32; ++i){
+//             sc_printf("%02hhX", *(ecdh_shared_key+i));
+//         }
+//         sc_printf("\n");
+//         ecdh_shared_key_exist = true;
+//     }
+// }
 
-void test_ecdh(uint8_t *pri_key, uint8_t *other_pub){
-    uint8_t shared_key[32];
-    generate_curve25519_shared_key(shared_key, pri_key, other_pub);
-    sc_printf("ECDH test, server public key is: \n");
-    for(int i = 0; i < 32; ++i){
-        sc_printf("%02hhX", *(other_pub+i));
-    }
-    sc_printf("\n");
-    sc_printf("The shared secret key is: \n");
-    for(int i = 0; i < 32; ++i){
-        sc_printf("%02hhX", *(shared_key+i));
-    }
-    sc_printf("\n");
-}
+// void test_ecdh(uint8_t *pri_key, uint8_t *other_pub){
+//     uint8_t shared_key[32];
+//     generate_curve25519_shared_key(shared_key, pri_key, other_pub);
+//     sc_printf("ECDH test, server public key is: \n");
+//     for(int i = 0; i < 32; ++i){
+//         sc_printf("%02hhX", *(other_pub+i));
+//     }
+//     sc_printf("\n");
+//     sc_printf("The shared secret key is: \n");
+//     for(int i = 0; i < 32; ++i){
+//         sc_printf("%02hhX", *(shared_key+i));
+//     }
+//     sc_printf("\n");
+// }
 
-void test_message(uint8_t *msg){
-    uint8_t front_offset = 9;
-    uint8_t msg_len = 8;
-    sc_printf("The message received is: \n");
-    for(int i = 0; i < msg_len; ++i){
-        sc_printf("%02hhX", *(msg+i));
-    }
-    sc_printf("\n");
-}
+// void test_message(uint8_t *msg){
+//     uint8_t front_offset = 9;
+//     uint8_t msg_len = 8;
+//     sc_printf("The message received is: \n");
+//     for(int i = 0; i < msg_len; ++i){
+//         sc_printf("%02hhX", *(msg+i));
+//     }
+//     sc_printf("\n");
+// }
 
-void string_to_byte_convert(char s[], uint8_t  addressBytes[32]) {
-    sscanf(s, "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx", &addressBytes[0], 
-    &addressBytes[1], &addressBytes[2], &addressBytes[3], &addressBytes[4],  \
-    &addressBytes[5], &addressBytes[6], &addressBytes[7], &addressBytes[8],  \
-    &addressBytes[9], &addressBytes[10], &addressBytes[11],&addressBytes[12],\
-    &addressBytes[13], &addressBytes[14], &addressBytes[15], &addressBytes[16],
-    &addressBytes[17], &addressBytes[18], &addressBytes[19], &addressBytes[20], 
-    &addressBytes[21], &addressBytes[22], &addressBytes[23], &addressBytes[24], 
-    &addressBytes[25], &addressBytes[26], &addressBytes[27], &addressBytes[28],
-    &addressBytes[29], &addressBytes[30], &addressBytes[31]);
-}
+// void string_to_byte_convert(char s[], uint8_t  addressBytes[32]) {
+//     sscanf(s, "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx", &addressBytes[0], 
+//     &addressBytes[1], &addressBytes[2], &addressBytes[3], &addressBytes[4],  \
+//     &addressBytes[5], &addressBytes[6], &addressBytes[7], &addressBytes[8],  \
+//     &addressBytes[9], &addressBytes[10], &addressBytes[11],&addressBytes[12],\
+//     &addressBytes[13], &addressBytes[14], &addressBytes[15], &addressBytes[16],
+//     &addressBytes[17], &addressBytes[18], &addressBytes[19], &addressBytes[20], 
+//     &addressBytes[21], &addressBytes[22], &addressBytes[23], &addressBytes[24], 
+//     &addressBytes[25], &addressBytes[26], &addressBytes[27], &addressBytes[28],
+//     &addressBytes[29], &addressBytes[30], &addressBytes[31]);
+// }
